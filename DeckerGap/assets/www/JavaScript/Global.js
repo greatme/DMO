055e7d630a32b5a51daa1f5ae11bd663c88859a5
DeckerCHAN <DeckerCHAN@gmail.com>
2013-08-01 02:24:47 +0800

    搭建前端框架

--- DeckerGap/assets/www/JavaScript/Global.js
+++ DeckerGap/assets/www/JavaScript/Global.js
@@ -3,19 +3,21 @@ $(document).ready(function ()
 {
 	window.DMO_Controler.Init();
 	window.DMO_Console.Init();
-	window.window.DMO_Account.Login();
+	window.DMO_Account.Login();
+	TEST_GetTestData();
 }
 );
 
 window.DMO_Config =
 {
+
 	RunLeavel : "D1",
 	Console :
 	{
 		ConsoleCotrolId : "Console",
-		Enable : "false"
+		Enable : true
 	}
-	
+
 }
 window.DMO_Controler =
 {
@@ -31,7 +33,7 @@ window.DMO_Controler =
 		default:
 			alert("Unexpected RunLeavelCode!");
 			break;
-			
+
 		}
 	},
 	LoadJS : function (_ScriptName)
@@ -42,11 +44,11 @@ window.DMO_Controler =
 			type : "GET",
 			dataType : "script",
 			async : false
-			
+
 		}
 		);
 	}
-	
+
 };
 
 window.DMO_Console =
@@ -57,39 +59,54 @@ window.DMO_Console =
 		{
 			this.Log = function (_Messege)
 			{
-				console.log(_Messege);
+				if (window.DMO_Config.Console.Enable)
+				{
+					console.log(_Messege);
+				}
 			};
 		}
 		else if (window.DMO_Config.RunLeavel == "D2")
 		{
-			//TODO: Add Web Console Context Here
+			$("body").append($("<div id='" + window.DMO_Config.Console.ConsoleCotrolId + "'style='width:80%;height:40%;position:absolute;right:0;bottom:0;background-color:yellow;color:blue;'></div>"));
+			this.Log = function (_Messege)
+			{
+				if (window.DMO_Config.Console.Enable)
+				{
+					$("#" + window.DMO_Config.Console.ConsoleCotrolId).append(_Messege + "<br/>");
+				}
+			}
+
 		}
 	}
-};
+}
 window.DMO_Account =
 {
-	Init : function ()
-	{},
+	Init : function ()  {},
 	Login : function (_username, _password)
 	{
-		var username = _username || "";
-		var password = _password || "";
+		var username = _username || " ";
+		var password = _password || " ";
+		document.cookie = "LastLoginUser"+"=;expires="+(new Date(0)).toGMTString();
+		document.cookie = "SysLogin"+"=;expires="+(new Date(0)).toGMTString();
+		document.cookie = "DomAuthSessId"+"=;expires="+(new Date(0)).toGMTString();
+		
 		$.ajax(
 		{
-			url : "http://10.80.230.199/names.nsf?open",
+			url : "http://10.80.230.199/names.nsf?Login",
 			type : "POST",
 			dataType : "html",
 			async : false,
 			data :
 			{
 				"%%ModDate" : "0000000000000000",
-				"Remote_Addr" : "10.81.229.24",
+				"Remote_Addr" : "10.81.229.9",
 				"Path" : "/ldfiles/ldimges/login/",
 				"jspath" : "/ldfiles/ldjs/",
 				"csspath" : "/ldfiles/ldcss/",
 				"imgepath" : "/ldfiles/ldimges/login/",
-				"Username" : $.url.encode("闫修春"),
-				"Password" : "123456",
+				"Username" : "13jyanxch",
+				"Password" : "12345s6",
+				"Submit" : "%B5%C7%C2%BC",
 				"RedirectTo" : "/officedata/ldconfig.nsf?open",
 				"$PublicAccess" : "1",
 				"reasonType" : "2",
@@ -97,35 +114,45 @@ window.DMO_Account =
 			},
 			success : function (_data)
 			{
-				window.DMO_Console.Log("LoginSuccess!");
+				if (_data.indexOf("/officedata/ldconfig.nsf/Homepage?OpenForm") != -1)
+				{
+					window.DMO_Console.Log("LoginSuccess!");
+				}
+				else
+				{
+						window.DMO_Console.Log("WrongPassword!");
+				}
+
 			},
-			error : function ()
-			{}
-			
+			error : function (_data)
+			{
+				window.DMO_Console.Log("Error!" + _data);
+			}
+
 		}
 		);
 	}
 }
 /* TEST Area */
-var TEST_GetTestData=function()
+var TEST_GetTestData = function ()
 {
-		$.ajax(
+	window.DMO_Console.Log("TEST_GetTestData");
+	$.ajax(
+	{
+		url : "http://10.80.230.199/officedata/fwnbffk.nsf/ToDoDocForHomepage?ReadViewEntries&RestrictToCategory=%u95EB%u4FEE%u6625/%u516C%u53F8%u9886%u5BFC/%u6C34%u7535%u5341%u4E09%u5C40/SINOHYDRO",
+		type : "GET",
+		cache:false,
+		dataType : "xml",
+		async : false,
+		success : function (_data)
 		{
-			url : "http://10.80.230.199/officedata/fwnbffk.nsf/ToDoDocForHomepage?ReadViewEntries&RestrictToCategory=%u95EB%u4FEE%u6625/%u516C%u53F8%u9886%u5BFC/%u6C34%u7535%u5341%u4E09%u5C40/SINOHYDRO",
-			type : "GET",
-			dataType : "xml",
-			async : false,
-			success : function (_data)
-			{
-				window.DMO_Console.Log(_data);
-			},
-			error : function ()
-			{}
-			
-		});
+			window.DMO_Console.Log(_data);
+		},
+		error : function (_data)
+		{
+			window.DMO_Console.Log(_data);
+		}
+
+	}
+	);
 }
-	
-	
-	
-	
-	
--- DeckerGap/src/com/example/deckergap/MainActivity.java
+++ DeckerGap/src/com/example/deckergap/MainActivity.java
@@ -6,7 +6,6 @@ package com.example.deckergap;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
-import android.webkit.JsPromptResult;
 import android.webkit.WebView;
 
 import org.apache.cordova.DroidGap;
@@ -16,7 +15,7 @@ public class MainActivity extends DroidGap
 	public void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
-		super.loadUrl("file:///android_asset/www/index.html");
+		super.loadUrl("file:///android_asset/www/index2.html");
 			
 		
 		
@@ -26,7 +25,7 @@ public class MainActivity extends DroidGap
 	{
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+		return true; 
 	}
 
 }
